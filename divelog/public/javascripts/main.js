a5836ce85eadc7a38294dda30e4b0e4dd08e7fac
Steren <steren.giannini@gmail.com>
2010-10-19 09:54:56 +0000

    Added map support for edition

--- divelog/public/javascripts/main.js
+++ divelog/public/javascripts/main.js
@@ -29,3 +29,46 @@ $(document).ready(function() {
 	$("#actionFeedbackMessage").delay(5000).fadeOut();
 
 });
+
+// Google Maps
+function initialize() {
+    var mapDiv = document.getElementById('map-canvas');
+    var myLatlng = new google.maps.LatLng(0, 0);
+    var myOptions = {
+      zoom: 1,
+      center: myLatlng,
+      mapTypeId: google.maps.MapTypeId.SATELLITE,
+      disableDefaultUI: true,
+      navigationControl: true
+    };
+    
+    var map = new google.maps.Map(mapDiv, myOptions);
+
+    function setLatLngInputValues(location) {
+  	  $("#spotLatitude").val(location.lat());
+	  $("#spotLongitude").val(location.lng());
+    }
+    
+    function placeMarker(location) {
+  	  var marker = new google.maps.Marker({
+  		  map: map,
+  	      position: location, 
+          draggable: true
+  	  });
+
+      google.maps.event.addListener(marker, 'dragend', function(mouseEvent) {
+    	  setLatLngInputValues(mouseEvent.latLng);
+    	  map.panTo(mouseEvent.latLng);
+      });
+  	  
+	  setLatLngInputValues(location);
+      map.panTo(location);
+    }
+    
+    google.maps.event.addListenerOnce(map, 'click', function(event) {
+        placeMarker(event.latLng);
+    });
+    
+  }
+
+  google.maps.event.addDomListener(window, 'load', initialize);
